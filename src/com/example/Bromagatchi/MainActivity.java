f745dc8a6d13c4950262dc484b292f1bfae595cd
sdc <github@software.unf.dk>
2014-07-15 15:05:26 +0200

    Version Alpha

--- src/com/example/Bromagatchi/MainActivity.java
+++ src/com/example/Bromagatchi/MainActivity.java
@@ -2,16 +2,24 @@ package com.example.Bromagatchi;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
+
+    int hp = 100;
+    int xp = 0;
+    int energy = 50;
+    double happiness = 100;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        Button exercise = (button) findViewById(R.id.)
 
     }
 }
