5a2266acd7c0d6b5371133b1c13c84921d25756f
sdc <github@software.unf.dk>
2014-07-16 10:21:42 +0200

    added jump

---
+++ src/com/example/Bromagatchi/jump.java
@@ -0,0 +1,15 @@
+package com.example.Bromagatchi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sdc on 7/16/14.
+ */
+public class jump extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.jumpsquats);
+    }
+}
