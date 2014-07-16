af1bc489d2016b3da3e4fc36d223a2e96a6cba99
sdc <github@software.unf.dk>
2014-07-16 14:48:28 +0200

    FINISH THE FUCKING JUMP

--- src/com/example/Bromagatchi/jump.java
+++ src/com/example/Bromagatchi/jump.java
@@ -3,6 +3,11 @@ package com.example.Bromagatchi;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
 import android.widget.ImageView;
 
 /**
@@ -16,68 +21,84 @@ public class jump extends Activity {
     public double hap = 1;
 
     private SharedPreferences prefs;
+    private ImageView broImage;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.jumpsquats);
+        broImage = (ImageView) findViewById(R.id.BroImage);
+        final Button jumpButton = (Button) findViewById(R.id.jumpButton);
+        jumpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation animationu = new TranslateAnimation(0, 0, 0, -400);
+                animationu.setDuration(1000);
+                animationu.setFillAfter(true);
+                broImage.startAnimation(animationu);
+
+
+                animationu.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        jumpButton.setEnabled(false);
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        Animation animationd = new TranslateAnimation(0, 0, -400, 0);
+                        animationd.setDuration(1000);
+                        animationd.setFillAfter(true);
+                        broImage.startAnimation(animationd);
+                        jumpButton.setEnabled(true);
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+                xp++;
+            }
+
+            public void update() {
+
+                if (xp < 5) {
+                    setImageBRO(R.drawable.phase0); //Ændre til BRO, så vi har en metode til at ændre baggrunden
+                    //Background skifte
+                }
+                if (xp > 4) {
+                    setImageBRO(R.drawable.phase1); //Ændre til BRO, så vi har en metode til at ændre baggrunden
+                    //Background skifte
+                }
+                if (xp > 30) {
+                    setImageBRO(R.drawable.phase2); //Ændre til BRO, så vi har en metode til at ændre baggrunden
+                    //Background skifte
+                }
+                if (xp > 50) {
+                    setImageBRO(R.drawable.phase3); //Ændre til BRO, så vi har en metode til at ændre baggrunden
+                    //Background skifte
+                }
+                if (xp > 100) {
+                    setImageBRO(R.drawable.phase4); //Ændre til BRO, så vi har en metode til at ændre baggrunden
+                    //Background skifte
+                }
+
+            }
+
+            public void setImageBRO(int image) {
+                broImage.setImageResource(image); //Ændre til BRO, så vi har en metode til at ændre baggrunden
+            }
+        });
     }
-
-    public void update() {
-
-        if (xp < 5) {
-            setImageBRO(R.drawable.phase0); //Ændre til BRO, så vi har en metode til at ændre baggrunden
-            //Background skifte
-        }
-        if (xp > 4) {
-            setImageBRO(R.drawable.phase1); //Ændre til BRO, så vi har en metode til at ændre baggrunden
-            //Background skifte
-        }
-        if (xp > 30) {
-            setImageBRO(R.drawable.phase2); //Ændre til BRO, så vi har en metode til at ændre baggrunden
-            //Background skifte
-        }
-        if (xp > 50) {
-            setImageBRO(R.drawable.phase3); //Ændre til BRO, så vi har en metode til at ændre baggrunden
-            //Background skifte
-        }
-        if (xp > 100) {
-            setImageBRO(R.drawable.phase4); //Ændre til BRO, så vi har en metode til at ændre baggrunden
-            //Background skifte
-        }
-
-    }
-
-
-    private ImageView broImage;
-    public void setImageBRO(int image) {
-        broImage.setImageResource(image); //Ændre til BRO, så vi har en metode til at ændre baggrunden
-    }
-
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        // Indlæs tiden appen blev paused
-        //long lastTime = prefs.getLong("lastTime", System.currentTimeMillis()); // Default værdi er System.currentTime...
-        //long diffrence = System.currentTimeMillis() - lastTime; // Difference i ms
-        //System.out.println(diffrence);
-        /*if (diffrence / 1000 > 60) //HVERT MINUT
-        {
-            hp += diffrence / 10000;
-            energy += diffrence / 10000; //10000 er blot en faktor
-        }
-        xp = prefs.getInt("XP on pause", xp);
-        if (energy >= 50) {
-            energy = 50;
-        }
-        if (hp >= 100) {
-            hp = 100;
-        }
-        update();*/
-    }
-
-
 }
+
+
+
+   /*) public void setYBROd() {
+        Animation animationd = new TranslateAnimation(0, 0, -400, 0);
+        animationd.setDuration(1000);
+        animationd.setFillAfter(true);
+        broImage.startAnimation(animationd);
+    }*/
